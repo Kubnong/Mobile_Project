d9d0273af7facd88685722f18d5a767bafe3f6c1
Kubnong <riw3566@gmail.com>
2025-02-11 03:51:50 +0700

    Finish Mobile_Project
    Add Feature Darkmode , Category , Edit , Stylesheet ,

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,70 +3,99 @@ import { Touchable } from "react-native";
 import { View, Text, StyleSheet, Image ,TouchableOpacity} from "react-native";
 import CheckBox from "react-native-check-box";
 
-const Card = ({image ,name , price , onDelete ,check ,onCheckChange}) => {
+const Card = ({image , name , price , edit, onDelete ,check ,onCheckChange ,category, textStyle}) => {
     return (
-        <View style={styles.card}>
+        <View style={[styles.card ,textStyle]}>
             <View>
-                {image}
-                <Text style={styles.title}>{name}</Text>
-                <Text style={styles.content}>{price}</Text>
-                <CheckBox
-                    style={styles.checkbox}
-                    isChecked={check}
-                    onClick={onCheckChange}
+                <Image
+                    source={{uri:image}}
+                    style={styles.image}
                 />
-                <Text style={styles.text}>Buy</Text>
+                <Text style={styles.title}>{name}</Text>
+                <Text style={styles.content}>price : {price}</Text>
+                <Text style={styles.content}>category : {category}</Text>
+                <View style={styles.containerCheckbox}>
+                    <CheckBox
+                        style={styles.checkbox}
+                        isChecked={check}
+                        onClick={onCheckChange}
+                    />
+                    <Text style={styles.text}>Buy</Text>
+                </View>
             </View>
-            <TouchableOpacity style={styles.deleteButton} onPress={onDelete}>
+            <View>
+                <TouchableOpacity style={styles.editButton} onPress={edit}>
+                    <Text style={styles.deleteText}>Edit</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.deleteButton} onPress={onDelete}>
                     <Text style={styles.deleteText}>X</Text>
-            </TouchableOpacity>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     card: {
+        width: '45%',
+        height: 250,
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'flex-start',
         backgroundColor: 'white',
-        padding: 20, // เพิ่มระยะห่างด้านในของการ์ด
+        borderColor: '#3674B5', 
+        borderWidth: 2,
+        padding: 10, // เพิ่มระยะห่างด้านในของการ์ด
         borderRadius: 10, // มุมของการ์ดโค้งมน 10 หน่วย
-        margin: 10,
+        margin: 11,
         shadowColor: '#000',
         shadowOffset: {width: 0, height: 2},
         shadowOpacity: 0.2,
         shadowRadius: 4,
-        elevation: 2,
+        elevation: 5,
     },
     title: {
-        fontSize: 18,
+        fontSize: 17,
         fontWeight: 'bold',
-        marginBottom: 10,
+        marginBottom: 4,
     },
     content: {
         fontSize: 14,
         color: '#555'
     },
-    image: {
-        width: 300,
-        height: 350,
-        resizeMode: 'cover', // ทำให้รูปภาพพอดีกับพื้นที่โดยคงอัตราส่วนเดิม
-        borderRadius: 10,
-    },
     text: {
-        fontSize: 18,
+        fontSize: 15,
+    },
+    editButton: {
+        backgroundColor: "grey",
+        padding: 5,
+        borderRadius: 5,
+        top: 200,
+        right: 18,
     },
     deleteButton: {
         backgroundColor: "red",
-        padding: 10,
+        padding: 5,
         borderRadius: 5,
+        right: 18,
+        top: -35,
+        alignItems: 'center', 
+        justifyContent: 'center'
     },
     deleteText: {
         color: "white",
         fontWeight: "bold",
     },
     checkbox: {
-        alignSelf: 'center',
+        width: 25,
     },
+    image: {
+        width: 150,
+        height: 140,
+        resizeMode:'center', // ทำให้รูปภาพพอดีกับพื้นที่โดยคงอัตราส่วนเดิม
+        borderRadius: 10,
+    },
+    containerCheckbox:{
+        flexDirection: 'row',
+    }
 })
 export default Card;
--- src/screens/Product.js
+++ src/screens/Product.js
@@ -1,29 +1,47 @@
 import React, {useState ,useEffect} from "react";
-import {View ,TextInput ,StyleSheet, TouchableOpacity ,Text ,Alert ,Modal ,FlatList} from "react-native";
+import {View ,TextInput ,StyleSheet, TouchableOpacity ,Text ,Modal ,FlatList ,ScrollView} from "react-native";
 import Card from "../components/Card";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { launchImageLibrary } from "react-native-image-picker";
+import CheckBox from "react-native-check-box";
+import { Switch } from "react-native-gesture-handler";
 
 const Product = () => {
     const [isVisible, setIsVisible] = useState(false)
-    const [image, setImage] = useState();
     const [name, setName] = useState("");
     const [price , setPrice] = useState("");
+    const [url , setUrl] = useState("");
     const [product, setProduct] = useState([]);
     const [sumPrice, setSumprice] = useState(0);
+    const [category, setCategory] = useState("");
     const [searchText, setSearchText] = useState("");
+    const [searchCategory, setSearchCategory] = useState("");
+    const [edit , setEdit] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(false);
     const STORAGE_KEY = '@product_data';
 
-    const addProduct = async () => {
-        if(!name.trim() || !price.trim()){
-            alert("กรุณากรอกชื่อสินค้า หรือราคาด้วย");
+    const addProduct = async () => { // เพิ่มสินค้า
+        if(!url.trim() || !name.trim() || !price.trim() || !category.trim()){
+            alert("กรุณากรอกข้อมูลให้ครบถ้วน");
             return ;
         }
-        const newProduct = { id:Date.now().toString(),name ,price ,image};
-        const updateProduct = [newProduct, ...product]
+        let updateProduct;
+
+        if(edit) {
+            updateProduct = product.map((item) =>
+                item.id === edit ? { ...item, url, name, price, category } : item
+            );
+        }
+        else {
+            const newProduct = { id:Date.now().toString(),url ,name ,price ,category};
+            updateProduct = [newProduct, ...product]
+        }
+        
         setProduct(updateProduct);
+        setUrl("");
         setName("");
         setPrice("");
+        setCategory("");
+        setEdit(false);
         calculateSumprice(updateProduct);
 
         try {
@@ -35,28 +53,46 @@ const Product = () => {
     };
 
     const loadProduct = async () => {
-        try{
+        try {
             const storedProduct = await AsyncStorage.getItem(STORAGE_KEY);
-            setProduct(JSON.parse(storedProduct));
-        } catch(error){
-            console.log("Failed to load",error)
+            setProduct(storedProduct ? JSON.parse(storedProduct) : []);
+            calculateSumprice(storedProduct ? JSON.parse(storedProduct) : []);
+        } catch (error) {
+            console.log("Failed to load", error);
         }
-    }
+    };
 
-    useEffect(() => {
+    useEffect(() => { // โหลดสินค้าเมื่อเปิดหน้าจอ
         loadProduct();
     }, []);
 
-    const filteredProduct = product.filter((product) => 
-        product.name.toLowerCase().includes(searchText.toLowerCase())
+    const filteredProduct = (product || []).filter((product) => 
+        product.name.toLowerCase().includes(searchText.toLowerCase()) &&
+        (searchCategory ? product.category === searchCategory : true)
     );
-
-    const deleteProduct = (name) => {
+    
+    const editProduct = (item) => {
+        setIsVisible(true);
+        setUrl(item.url);
+        setName(item.name);
+        setPrice(item.price);
+        setCategory(item.category);
+        setEdit(item.id);
+    };
+    
+    const deleteProduct = async (name) => { // ลบสินค้า
         const newList = product.filter((item) => item.name != name);
         setProduct(newList);
         calculateSumprice(newList);
+
+        try {
+            await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(newList));
+        } catch (error) {
+            console.log("Error deleting product:", error);
+        }
     };
-    const toggleCheck = (id) => {
+
+    const toggleCheck = (id) => { // เปลี่ยนสถานะของสินค้า
         const updatedProducts = product.map((item) => {
             if (item.id === id) {
                 return { ...item, check: !item.check };
@@ -66,7 +102,7 @@ const Product = () => {
         setProduct(updatedProducts);
         calculateSumprice(updatedProducts);
     };
-    const calculateSumprice = (product) => {
+    const calculateSumprice = (product) => { // คำนวณผลรวมของราคาสินค้า
         const total = product.reduce((sum, item) => {
             if (item.check) {
                 return sum ;
@@ -78,87 +114,105 @@ const Product = () => {
         setSumprice(total);
     };
     
-    const openGallery = () => {
-        const options = {
-            mediaType: "photo",
-            selectionLimit: 1,
+    const clearAllProduct = async () => { // ลบสินค้าทั้งหมด
+        setProduct([]);
+        setSumprice(0);
+        try {
+            await AsyncStorage.removeItem(STORAGE_KEY);
+        } catch (error) {
+            console.log("Error clearing all product:", error);
         }
-        launchImageLibrary( options, (response) => {
-            if(response,didCancel){
-                console.log("Cancel Gallery");
-            } else if(response.errorMessage){
-                console.log("Gallery Error : ",response.errorMessage);
-            } else {
-                console.log('Selected Image URI : ', response.assets[0].uri);
-                const uri = response.assets[0].uri;
-                setImage(uri);
-            }
-        });
-    };
-
+    }
+    const toggleTheme = () => {
+        setIsDarkMode((prev) => !prev);
+    }
     return(
-        <View style = {styles.container}>
-            <TextInput
-                style = {styles.input}
-                placeholder="Search"
-                value={searchText}
-                onChangeText={setSearchText}
-            />
+        <View style = {[styles.container, {backgroundColor: isDarkMode ? 'black' : '#D1F8EF'}]}>
+            <Switch value={isDarkMode} onValueChange={toggleTheme}></Switch>
+            <View style = {styles.searchContainer}>
+                <TextInput
+                    style={styles.font}
+                    placeholder="🔍  Search"
+                    value={searchText}
+                    onChangeText={setSearchText}
+                />
+            </View>
+            <View style={styles.categoryContainer}>
+                <TouchableOpacity style={styles.buttonCategory}>
+                    <Text style={styles.font} onPress={() => setSearchCategory("")}>All</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonCategory}>
+                    <Text style={styles.font} onPress={() => setSearchCategory("Food")}>Food</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonCategory}>
+                    <Text style={styles.font} onPress={() => setSearchCategory("Electronic")}>Electronic</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonCategory}>
+                    <Text style={styles.font} onPress={() => setSearchCategory("Health")}>Health</Text>
+                </TouchableOpacity>
+            </View>
             <FlatList
                 data={filteredProduct}
                 keyExtractor={(item) => item.id}
+                numColumns={2}
+                columnWrapperStyle={{justifyContent: 'space-between'}}
                 renderItem={ ({item}) => {
                     return (
                         <Card
-                            image = {item.image}
+                            image = {item.url}
                             name = {item.name}
                             price = {item.price}
                             check = {item.check}
+                            edit = {() => editProduct(item)} 
                             onDelete={() => deleteProduct(item.name)} // ส่งฟังก์ชันลบไปให้ Card
                             onCheckChange={() => toggleCheck(item.id)} // ส่งฟังก์ชันเปลี่ยนสถานะไปให้ Card
+                            category={item.category}
+                            textStyle={item.check ? styles.purchasedItem : null} // ส่ง style ไปให้ Card
                         />
                     )
                 }}
             />
-            <View>
-                <Text>ผลรวมของราคาสินค้า = {sumPrice} </Text>
+            <View style={{padding: 1 , alignItems: 'center'}}>
+                <Text style={[styles.fontSumprice,{ color: isDarkMode ? "#fff" : "#000", fontSize: 18 }]}>ผลรวมของราคาสินค้าที่ยังไม่ได้ซื้อ = {sumPrice} </Text>
             </View>
-            <View style={styles.closebuttonContainer}>
+            <View style={styles.containerButton}>
                 <TouchableOpacity 
-                    style={styles.clearButton}
-                    onPress={ () => {setProduct([]) , setSumprice(0)} }
-                >
-                    <Text>Clear All</Text>
-                </TouchableOpacity>
-            </View>
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity 
-                    style={styles.button}
+                    style={styles.addProductButton}
                     onPress={() => setIsVisible(true)}
                 >
-                    <Text>Add Product</Text>
+                    <Text style={{fontWeight:'bold' , color:'white'}}>Add Product</Text>
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    style={styles.clearProductButton}
+                    onPress={clearAllProduct}
+                >
+                    <Text style={{fontWeight:'bold' , color:'white'}}>Clear All</Text>
                 </TouchableOpacity>
             </View> 
             <Modal visible={isVisible} transparent ={true} animationType="slide">
                 <View style={styles.modalContainer}>
                     <View style={styles.modalContent}>
-                        <Text>Add Product</Text>
-                        <View style={styles.closeButton}>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 10 }}>
+                            <Text style={{ fontSize: 18, fontWeight: 'bold', textAlign: 'center', flex: 1 }}>Add Product</Text>
                             <TouchableOpacity 
+                                style={styles.closeButton}
                                 onPress={() => {
+                                    setUrl("");
+                                    setName("");
+                                    setPrice("");
+                                    setCategory("");
                                     setIsVisible(false);
                                 }}
                             >
                                 <Text style={styles.buttonText}>X</Text>
                             </TouchableOpacity>
                         </View>
-                        <TouchableOpacity
-                            style={styles.clearButton}
-                            onPress={openGallery}
-                        >
-                            <Text>Select Image</Text>
-                        </TouchableOpacity>
-                        {image}
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Image URL"
+                            value={url}
+                            onChangeText={setUrl} 
+                        />
                         <TextInput
                             style={styles.input}
                             placeholder="ชื่อสินค้า"
@@ -167,13 +221,39 @@ const Product = () => {
                         />
                         <TextInput
                             style={styles.input}
-                            //keyboardType="decimal-pad"
+                            keyboardType="numeric"
                             placeholder="ราคาสินค้า"
                             value={price}
                             onChangeText={setPrice}
                         />
-                        <TouchableOpacity 
-                            onPress={() => {addProduct}}
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                <CheckBox
+                                    isChecked={category === "Food"}
+                                    onClick={() => setCategory("Food")}
+                                />
+                                <Text style={{ marginLeft: 5 }}>Food</Text>
+                            </View>
+
+                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                <CheckBox
+                                    isChecked={category === "Electronic"}
+                                    onClick={() => setCategory("Electronic")}
+                                />
+                                <Text style={{ marginLeft: 5 }}>Electronic</Text>
+                            </View>
+
+                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                <CheckBox
+                                    isChecked={category === "Health"}
+                                    onClick={() => setCategory("Health")}
+                                />
+                                <Text style={{ marginLeft: 5 }}>Health</Text>
+                            </View>
+                        </View>
+                        <TouchableOpacity
+                            style={styles.addProductModal} 
+                            onPress={addProduct}
                         >
                             <Text>Add Product</Text>
                         </TouchableOpacity>
@@ -186,14 +266,37 @@ const Product = () => {
 const styles = StyleSheet.create({
     container:{
         flex: 1,
+        backgroundColor: '#D1F8EF'
     },
-    input: {
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 5,
+    searchContainer: {
+        flexDirection: 'row',
+        backgroundColor: 'white', 
+        borderRadius: 20,
+        paddingHorizontal: 10,
+        margin: 10,
+    },
+    font: {
+       fontSize: 14,
+       fontWeight: 'bold',
+       color: 'white',
+    },
+    fontSumprice: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        color: 'black',
+     },
+    categoryContainer: {
+        paddingHorizontal: 10, // ปรับให้มีพื้นที่กดง่ายขึ้น
+        flexDirection: 'row',
+    },
+    buttonCategory: {
+        backgroundColor: '#3674B5',
+        alignItems: 'center', 
+        justifyContent: 'center',
+        borderRadius: 20,
         padding: 10,
-        fontSize: 18,
-        margin: 5
+        width: 88,
+        margin: 5,
     },
     buttonContainer: { // Style สำหรับ View ที่ครอบปุ่ม
         position: 'absolute',
@@ -201,19 +304,28 @@ const styles = StyleSheet.create({
         left: 20,   // ระยะห่างจากขอบซ้าย
         right: 20,  // ระยะห่างจากขอบขวา (เพื่อให้ปุ่มเต็มความกว้าง)
     },
-    closebuttonContainer: { // Style สำหรับ View ที่ครอบปุ่ม
-        position: 'absolute',
-        bottom: 80, // ระยะห่างจากขอบล่าง
-        left: 20,   // ระยะห่างจากขอบซ้าย
-        right: 20,  // ระยะห่างจากขอบขวา (เพื่อให้ปุ่มเต็มความกว้าง)
+    containerButton: {
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
-    button: {
+    addProductButton: {
         backgroundColor: 'green',
-        paadding: 10,
+        padding: 10,
         alignItems: 'center',
         justifyContent: 'center',
         borderRadius: 10,
-        width: 370,
+        width: 300,
+        height: 50
+    },
+    clearProductButton:{
+        alignSelf: "flex-start",
+        alignItems: 'center', 
+        justifyContent: 'center',
+        backgroundColor: 'red',
+        paadding: 30,
+        borderRadius: 10,
+        width: 90,
         height: 50
     },
     modalContainer: {
@@ -223,28 +335,36 @@ const styles = StyleSheet.create({
         backgroundColor: "rgba(0,0,0,0.5)",
     },
     modalContent: {
-        width: 300,
-        height: 400,
+        width: 320,
         backgroundColor: "white",
         padding: 20,
         borderRadius: 10,
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
+        elevation: 5,
         alignItems: "center",
     },
     input: {
+        width: "100%", // ให้กว้างเท่ากับ modalContent
         borderWidth: 1,
         borderColor: '#ccc',
         borderRadius: 5,
         padding: 10,
-        fontSize: 18,
-        margin: 5
+        fontSize: 14,
+        marginVertical: 8, // เพิ่มระยะห่างแนวตั้ง
+        backgroundColor: "#f9f9f9",
     },
     closeButton: {
-        alignSelf: "flex-start",
-        backgroundColor: 'grey',
-        paadding: 10,
-        borderRadius: 10,
-        width: 35,
-        height: 35
+        alignItems: 'center', 
+        justifyContent: 'center',
+        backgroundColor: 'red',
+        borderRadius: 15,
+        width: 30,
+        height: 30,
+        position: 'absolute',
+        right: 260,
+        top: -5,
     },
     buttonText:{
         color: 'white',
@@ -253,14 +373,30 @@ const styles = StyleSheet.create({
         alignItems: 'center', 
         justifyContent: 'center'
     },
-    clearButton:{
-        alignSelf: "flex-start",
-        backgroundColor: 'grey',
-        paadding: 30,
+    checkboxContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "100%", // จัดเรียงให้พอดีกับ modalContent
+        marginVertical: 8,
+    },
+    text: {
+        fontSize: 18,
+    },
+    addProductModal:{
+        backgroundColor: 'green',
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
         borderRadius: 10,
-        width: 200,
-        height: 50
-    }
+        width: 275,
+        height: 40,
+        margin: 1, 
+    },
+    purchasedItem: {
+        textDecorationLine: 'line-through',
+        opacity: 0.5, // ทำให้สีจางลง
+    },
 })
 
 export default Product;
